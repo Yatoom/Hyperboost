cfe968f926781b8948d61263b3903552af3061d8
Yatoom <jmapvhoof@gmail.com>
2019-04-19 22:02:14 +0200

    Patched SMAC with batching neighbors and making Pynisher optional.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # from os import path
 from setuptools import setup
+
 import hyperboost
 
 # Get the long description from the README file
@@ -7,6 +8,12 @@ import hyperboost
 # with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 #     long_description = f.read()
 
+with open('requirements.txt') as fh:
+    requirements = fh.read()
+requirements = requirements.split('\n')
+requirements = [requirement.strip() for requirement in requirements]
+
+print(requirements)
 setup(
     name='hyperboost',
     version=hyperboost.__version__,
@@ -17,5 +24,5 @@ setup(
     author_email='jmapvhoof@gmail.com',
     description='A hyperparameter optimization tool based on SMAC and gradient boosting.',
     # long_description=long_description,
-    install_requires=["numpy", "lightgbm", "sklearn", "pandas", "smac", "openml", "ConfigSpace"],
+    install_requires=requirements
 )
