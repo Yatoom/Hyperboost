6817416a37462fcae0a97b4fe24b277d02eea6fb
Yatoom <jmapvhoof@gmail.com>
2019-04-25 02:17:26 +0200

    Added dual ranking for comparing two methods, and visualization of std.

--- benchmarks/visualize_trajectory.py
+++ benchmarks/visualize_trajectory.py
@@ -31,12 +31,45 @@ def mean_of_runs(data):
 
     return result
 
+def dual_rank(a, b):
+    a = np.array(a)
+    b = np.array(b)
+    a_res = np.zeros_like(a)
+    b_res = np.zeros_like(b)
+    for i in range(a.shape[0]):
+        for j in range(a.shape[1]):
+            val_a = a[i, j]
+            val_b = b[i, j]
+            a_res[i, j] = 1 if val_a < val_b else 1.5 if val_a == val_b else 2
+            b_res[i, j] = 1 if val_b < val_a else 1.5 if val_a == val_b else 2
+    return a_res, b_res
 
-def mean_of_datasets(mean_of_runs):
-    smac_train = [i['smac']['loss_train'] for i in mean_of_runs.values()]
-    hyperboost_train = [i['hyperboost']['loss_train'] for i in mean_of_runs.values()]
-    smac_test = [i['smac']['loss_test'] for i in mean_of_runs.values()]
-    hyperboost_test = [i['hyperboost']['loss_test'] for i in mean_of_runs.values()]
+
+def rank_against(mean_runs):
+    smac_train = [i['smac']['loss_train'] for i in mean_runs.values()]
+    hyperboost_train = [i['hyperboost']['loss_train'] for i in mean_runs.values()]
+    smac_test = [i['smac']['loss_test'] for i in mean_runs.values()]
+    hyperboost_test = [i['hyperboost']['loss_test'] for i in mean_runs.values()]
+
+    smac_train, hyperboost_train = dual_rank(smac_train, hyperboost_train)
+    smac_test, hyperboost_test = dual_rank(smac_test, hyperboost_test)
+
+    return {
+        "smac_train_mean": np.mean(smac_train, axis=0),
+        "smac_train_std": np.std(smac_train, axis=0),
+        "smac_test_mean": np.mean(smac_test, axis=0),
+        "smac_test_std": np.std(smac_test, axis=0),
+        "hyperboost_train_mean": np.mean(hyperboost_train, axis=0),
+        "hyperboost_train_std": np.std(hyperboost_train, axis=0),
+        "hyperboost_test_mean": np.mean(hyperboost_test, axis=0),
+        "hyperboost_test_std": np.std(hyperboost_test, axis=0),
+    }
+
+def mean_of_datasets(mean_runs):
+    smac_train = [i['smac']['loss_train'] for i in mean_runs.values()]
+    hyperboost_train = [i['hyperboost']['loss_train'] for i in mean_runs.values()]
+    smac_test = [i['smac']['loss_test'] for i in mean_runs.values()]
+    hyperboost_test = [i['hyperboost']['loss_test'] for i in mean_runs.values()]
 
     return {
         "smac_train_mean": np.mean(smac_train, axis=0),
@@ -56,16 +89,21 @@ if __name__ == "__main__":
         with open(os.path.join(dir, file), "r") as f:
             data = json.load(f)
             r = mean_of_runs(data)
-            r = mean_of_datasets(r)
+            r = rank_against(r)
+            # r = mean_of_datasets(r)
             results[seed] = r
 
     results = [results[str(i)] for i in config.SEEDS if str(i) in results.keys()]
     if in_progress:
         results = results[:-1]
     frames = [pd.DataFrame(i) for i in results]
-    frame = pd.concat(frames).groupby(level=0).mean().iloc[1:]
-    frame["smac_train_mean"].plot()
-    frame["hyperboost_train_mean"].plot()
+    mean = pd.concat(frames).groupby(level=0).mean().iloc[1:]
+    std = pd.concat(frames).groupby(level=0).std().iloc[1:]
+    mean["smac_train_mean"].plot()
+    mean["hyperboost_train_mean"].plot()
+    plt.fill_between(np.arange(mean["smac_train_mean"].shape[0]), mean["smac_train_mean"] - std["smac_train_mean"], mean["smac_train_mean"] + std["smac_train_mean"], alpha=0.5)
+    plt.fill_between(np.arange(mean["hyperboost_train_mean"].shape[0]), mean["hyperboost_train_mean"] - std["hyperboost_train_mean"],
+                     mean["hyperboost_train_mean"] + std["hyperboost_train_mean"], alpha=0.5)
     plt.legend()
     plt.show()
     print()
