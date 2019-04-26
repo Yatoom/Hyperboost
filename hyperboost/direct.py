e4d73010c236a5416e695e65dc6384cdb1a8f9ee
Yatoom <jmapvhoof@gmail.com>
2019-04-26 18:12:10 +0200

    Extra options for Hyperboost.

--- hyperboost/direct.py
+++ hyperboost/direct.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from smac.epm.base_epm import AbstractEPM
 from smac.optimizer.acquisition import AbstractAcquisitionFunction
 
@@ -11,3 +12,13 @@ class Direct(AbstractAcquisitionFunction):
     def _compute(self, X: np.ndarray, **kwargs):
         loss, closeness = self.model.predict(X)
         return 1 - (loss + closeness) / 2
+
+
+class DirectNoDistance(AbstractAcquisitionFunction):
+    def __init__(self, model: AbstractEPM = None, **kwargs):
+        super().__init__(model)
+        self.long_name = 'Direct'
+
+    def _compute(self, X: np.ndarray, **kwargs):
+        loss, closeness = self.model.predict(X)
+        return 1 - loss
--- hyperboost/hyperboost.py
+++ hyperboost/hyperboost.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from hyperboost.direct import Direct
+from hyperboost.direct import Direct, DirectNoDistance
+from hyperboost.lightdropepm import LightDropEPM
 from hyperboost.lightepm import LightEPM
 from smac.facade.smac_facade import SMAC
 from smac.scenario.scenario import Scenario
@@ -9,10 +10,22 @@ from smac.utils.util_funcs import get_types
 
 
 class Hyperboost(SMAC):
-    def __init__(self, scenario: Scenario, rng: np.random.RandomState = None, **kwargs):
+    def __init__(self, scenario: Scenario, rng: np.random.RandomState = None, method="drop", **kwargs):
         types, bounds = get_types(scenario.cs, scenario.feature_array)
-        model = LightEPM(types=types, bounds=bounds, instance_features=scenario.feature_array,
-                         seed=rng.randint(MAXINT), pca_components=scenario.PCA_DIM)
-        acquisition_function = Direct(model=model)
-        super().__init__(scenario=scenario, rng=rng, model=model, acquisition_function=acquisition_function,
-                         use_pynisher=False, **kwargs)
+
+        if method == "drop":
+            model = LightDropEPM(types=types, bounds=bounds, instance_features=scenario.feature_array,
+                                 seed=rng.randint(MAXINT), pca_components=scenario.PCA_DIM)
+            super().__init__(scenario=scenario, rng=rng, model=model, use_pynisher=False, **kwargs)
+        elif method == "QRD":
+            model = LightEPM(types=types, bounds=bounds, instance_features=scenario.feature_array,
+                             seed=rng.randint(MAXINT), pca_components=scenario.PCA_DIM)
+            acquisition_function = Direct(model=model)
+            super().__init__(scenario=scenario, rng=rng, model=model, use_pynisher=False,
+                             acquisition_function=acquisition_function, **kwargs)
+        elif method == "QR":
+            model = LightEPM(types=types, bounds=bounds, instance_features=scenario.feature_array,
+                             seed=rng.randint(MAXINT), pca_components=scenario.PCA_DIM)
+            acquisition_function = DirectNoDistance(model=model)
+            super().__init__(scenario=scenario, rng=rng, model=model, use_pynisher=False,
+                             acquisition_function=acquisition_function, **kwargs)
