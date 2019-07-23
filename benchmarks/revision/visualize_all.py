da24ebab592f288d0f7869503ac307d4104f0c39
Yatoom <jmapvhoof@gmail.com>
2019-07-23 19:04:53 +0200

    Visualize revised runs.

---
+++ benchmarks/revision/visualize_all.py
@@ -0,0 +1,84 @@
+import json
+import os
+import re
+import pandas as pd
+import numpy as np
+import benchmark_visualize.util as util
+import matplotlib.pyplot as plt
+
+plt.style.use("seaborn")
+DIR = "."
+
+
+def rename(i):
+    n = i.replace("hyperboost", "Hyperboost")
+    n = n.replace("ei2", "EI")
+    n = n.replace("_2x", " 2x")
+    n = n.replace("_mean_train", "")
+    n = n.replace("_mean_test", "")
+    n = n.replace("roar", "ROAR")
+    n = n.replace("smac", "SMAC")
+    n = n.replace("random", "Random")
+    n = n.replace("-std-y", "")
+    n = n.replace("pca", "PCA")
+    return n
+
+
+grouped = {}
+for file in os.listdir(DIR):
+    if file.startswith("ALL-"):
+        postfix = re.search(r"[0-9]+", file)[0]
+        base = file.replace(f"-{postfix}", "")
+        base = base.replace(".json", "")
+        base = base.replace("ALL-", "")
+
+        if base not in grouped.keys():
+            grouped[base] = []
+
+        with open(os.path.join(DIR, file), "r") as f:
+            data = json.load(f)
+
+        grouped[base].append(data)
+
+print(grouped.keys())
+
+# Taking the mean of each run
+for group in grouped.keys():  # SVM, DecisionTree, RandomForest
+    print(group)
+    for seed in range(len(grouped[group])):  # 0, 1, 2
+
+        # Taking the mean per iteration: mean per run
+        mean_runs = util.mean_of_runs(grouped[group][seed])
+
+        # Taking the mean over all dataset: mean per method
+        grouped[group][seed] = util.mean_of_datasets(mean_runs)
+
+    # Taking the mean over all seeds
+    frames = [pd.DataFrame(i) for i in grouped[group]]
+    mean = pd.concat(frames).groupby(level=0).mean().iloc[1:]
+    std = pd.concat(frames).groupby(level=0).std().iloc[1:]
+
+    # Visualize train
+    columns = [i for i in mean.columns if "mean_test" in i]
+
+    max_100 = 0
+    min_100 = 1
+    renamed_columns = []
+    for i in columns:
+        n = rename(i)
+
+        if n in ["ROAR", "Hyperboost-PCA", "Hyperboost-EI"]:
+            continue
+
+        max_100 = max(max_100, mean[i].iloc[100] + 2 * std[i].iloc[100])
+        min_100 = min(min_100, mean[i].iloc[247] - 2 * std[i].iloc[247])
+        renamed_columns.append(n)
+        mean[i].plot()
+        plt.fill_between(np.arange(mean[i].shape[0]), mean[i] - std[i], mean[i] + std[i], alpha=0.5)
+    # plt.ylim(bottom=min_100, top=max_100)
+    plt.xlabel("Iteration")
+    plt.ylabel("1 - mean accuracy")
+    plt.legend(renamed_columns)
+    plt.show()
+
+print()
